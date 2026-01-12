d31b8679996e7bc9524a7460ca2291fffcdf41bc
ABHISHEK44444 <153176102+ABHISHEK44444@users.noreply.github.com>
2026-01-12 13:03:57 +0530

    Update Task.ts

--- backend/models/Task.ts
+++ backend/models/Task.ts
@@ -5,7 +5,6 @@ export interface ITask extends Document {
   userId: mongoose.Types.ObjectId;
   title: string;
   description: string;
-  priority: 'High' | 'Medium' | 'Low';
   status: 'Pending' | 'In Progress' | 'Completed' | 'Archived';
   dueDate: Date;
   notes?: string;
@@ -15,7 +14,6 @@ const TaskSchema: Schema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   title: { type: String, required: true },
   description: { type: String },
-  priority: { type: String, enum: ['High', 'Medium', 'Low'], default: 'Medium' },
   status: { type: String, enum: ['Pending', 'In Progress', 'Completed', 'Archived'], default: 'Pending' },
   dueDate: { type: Date, required: true },
   notes: { type: String },
